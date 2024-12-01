0eb2b60d72db44fe8a68fe76c0723876fbed93be
Aulinan <auroradepasquale@gmail.com>
2024-12-01 21:24:43 +0100

    Migrato a Svelte Flow ma per davvero

---
+++ Genix/src/Custom/CustomNode.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+    import { Handle, Position, type NodeProps } from '@xyflow/svelte';
+   
+    type $$Props = NodeProps;
+   
+    export let isConnectable: $$Props['isConnectable'];
+    export let data: $$Props['data'];
+  
+
+    const {label} = data
+  
+    
+  </script>
+  
+  <div class='customNode'>
+      <span>{label}</span>
+  <Handle type='source' position={Position.Right} id='right'  style="background: #555;" {isConnectable} />
+    <Handle
+      type="target"
+      position={Position.Left}
+      id="left"
+      style="background: #555;"
+      {isConnectable}
+    />
+    <Handle
+      type="target"
+      position={Position.Top}
+      id="top"
+      style="background: #555;"
+      {isConnectable}
+    />
+    <Handle
+    type="source"
+    position={Position.Bottom}
+    id="bottom"
+    style="background: #555;"
+    {isConnectable}
+    />
+  </div>
+
+   
+  <style>
+      .customNode {
+    background: #eee;
+    border-radius: 0.125rem;
+    height: 100px;
+    width: 100px;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+  
+  }
+  </style>
---
+++ Genix/src/Flow.svelte
@@ -0,0 +1,142 @@
+<script lang="ts">
+    import { writable } from 'svelte/store';
+    import {
+      SvelteFlow,
+      useSvelteFlow,
+      Background,
+      type Edge,
+      type Node,
+      type OnConnectEnd,
+      type OnConnectStart,
+      ConnectionLineType,
+      MiniMap,
+      Controls,
+
+
+    } from '@xyflow/svelte';
+   
+    import '@xyflow/svelte/dist/style.css';
+
+    import CustomNode from './Custom/CustomNode.svelte'
+
+    const snapGrid: [number, number] = [25, 25];
+
+    const nodeTypes = {
+        customNode : CustomNode
+    }
+   
+    const initialNodes: Node[] = [
+      {
+        id: '0',
+        type: 'customNode',
+        data: { label: 'Node' },
+        position: { x: 0, y: 0 },
+        origin : [0.5,0.5]
+      }
+    ];
+   
+    const nodes = writable<Node[]>(initialNodes);
+    const edges = writable<Edge[]>([]);
+   
+    let rect: DOMRectReadOnly;
+    let id = 1;
+    const getId = () => `${id++}`;
+   
+    const { screenToFlowPosition } = useSvelteFlow();
+
+    const handleConnectStart: OnConnectStart = (event,params)=> {
+      console.log('connection...',event,params)
+   
+
+    }
+   
+    const handleConnectEnd: OnConnectEnd = (event, connectionState) => {
+      if (connectionState.isValid) return;
+   
+      const initiatingNodeId = connectionState.fromNode?.id ?? '1';
+      const initiatingHandleId = connectionState.fromHandle?.id ?? 'right';
+      const sourceOrTarget = connectionState.fromHandle?.type
+      console.log('The Source Node Is ', initiatingNodeId, connectionState)
+      const id = getId();
+      const { clientX, clientY } = 'changedTouches' in event ? event.changedTouches[0] : event;
+   
+      const newNode: Node = {
+        id,
+        type:'customNode',
+        data: { label: `Node ${id}` },
+        // project the screen coordinates to pane coordinates
+        position: screenToFlowPosition({
+          x: clientX,
+          y: clientY
+        }),
+        // set the origin of the new node so it is centered
+        origin: [0.5, 0.5]
+      };
+      $nodes.push(newNode);
+      if (sourceOrTarget === 'source')
+        $edges.push({
+          source: initiatingNodeId,
+          target: id,
+          sourceHandle: initiatingHandleId ,
+          targetHandle: initiatingHandleId === 'right' ? 'left' : 'top',
+          id: `${initiatingNodeId}--${id}`
+        });
+      else
+      $edges.push({
+          source: id,
+          target: initiatingNodeId,
+          sourceHandle: initiatingHandleId==='left' ? 'right' : 'bottom',
+          targetHandle: initiatingHandleId,
+          id: `${id}--${initiatingNodeId}`
+        });
+      $nodes = $nodes;
+      $edges = $edges;
+    };
+  </script>
+   
+  <svelte:window />
+   
+  <div class="wrapper" bind:contentRect={rect}>
+    <SvelteFlow
+      {nodes}
+      {edges}
+      {nodeTypes}
+      {snapGrid}
+      fitView={true}
+      onconnectend={handleConnectEnd}
+      onconnectstart={handleConnectStart}
+      connectionLineType={ConnectionLineType.SmoothStep}
+      defaultEdgeOptions={{ type: 'smoothstep'}}
+      minZoom={1}
+      maxZoom={10}
+      
+    >
+        <Controls />
+        <MiniMap />
+      <Background />
+    </SvelteFlow>
+  </div>
+   
+  <style>
+   
+    :global(.svelte-flow .svelte-flow__edge path, .svelte-flow__connectionline path) {
+      stroke-width: 2;
+    }
+   
+    .wrapper {
+      height: 100vh;
+      width: 100vw;
+
+    }
+
+    :global(.svelte-flow__handle) {
+      opacity: 0;
+      /*TODO Animare Opacità*/
+      pointer-events: none;
+    }
+
+    :global(.svelte-flow__handle:hover) {
+      opacity: 1;
+      pointer-events: all;
+    }
+  </style>
