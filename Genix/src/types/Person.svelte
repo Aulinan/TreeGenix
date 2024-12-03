0d08e93c9815a40772906f9b139eeff9291f8c01
Aulinan <auroradepasquale@gmail.com>
2024-12-03 21:14:58 +0100

    Added Type Person

---
+++ Genix/src/types/Person.svelte
@@ -0,0 +1,12 @@
+<script lang="ts" context="module">
+	export type Person = Readonly<Partial<{
+    name:string;
+    dateOfBirth:Date;
+    age:number; // accepts float, can get from dateOfBirth
+    gender:Gender;
+    ailments:Array<string>;
+    deceased:boolean;
+    }>>
+    type Gender = 'male'|'female'
+
+</script>
