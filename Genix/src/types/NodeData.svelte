ea417a5ffc61450492c38c2c3798c8e8d32b6374
Aulinan <auroradepasquale@gmail.com>
2024-12-03 21:16:13 +0100

    Added type NodeData

---
+++ Genix/src/types/NodeData.svelte
@@ -0,0 +1,11 @@
+<script lang="ts" context="module">
+    import Person from './Person.svelte';
+
+    export type NodeData = {
+        id:string;
+        position:{x:number,y:number};
+        connections:string;
+        person:Person;
+    }
+
+</script>
